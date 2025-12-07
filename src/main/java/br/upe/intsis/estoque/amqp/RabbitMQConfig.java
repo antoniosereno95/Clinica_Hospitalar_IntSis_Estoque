c6409fe9f7c25dba0939dbadb20031ff80bfd4cc
Antonio Sereno <antoniojsereno@gmail.com>
2025-12-07 16:05:20 -0300

    Configurando Keys e Fila de Recebimento da Solicitação do Consultas
    Criação de uma nova fila, keys e exgange para a minha

--- src/main/java/br/upe/intsis/estoque/amqp/RabbitMQConfig.java
+++ src/main/java/br/upe/intsis/estoque/amqp/RabbitMQConfig.java
@@ -1,10 +1,6 @@
 package br.upe.intsis.estoque.amqp;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,7 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    //Nomes das Filas
+    //Conversor de mensagens JSON (Jackson)
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //Nomes das Filas Exemplo
     public static final String QUEUE_RECEBIMENTO = "fila.recebimento.json";
     public static final String QUEUE_ENVIO = "fila.envio.json";
     public static final String EXCHANGE_NAME = "meu.exchange";
@@ -44,17 +46,13 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(recebimentoQueue).to(exchange).with(QUEUE_RECEBIMENTO); // Usa o nome da fila como Routing Key
     }
 
-    //iga a Fila de Envio ao Exchange
+    //Liga a Fila de Envio ao Exchange
     @Bean
     public Binding envioBinding(Queue envioQueue, DirectExchange exchange) {
         return BindingBuilder.bind(envioQueue).to(exchange).with(QUEUE_ENVIO);
     }
 
-    //Conversor de mensagens JSON (Jackson)
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+
 
     //RabbitTemplate para usar o conversor JSON
     @Bean
@@ -63,4 +61,37 @@ public class RabbitMQConfig {
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
+
+
+    // -----------------------------################----------------------------------
+    //Fila de RECEBIMENTO DA SOLICITAsAO DE MATERIAIS DO SISTEMA DE CONSULTAS
+    // -----------------------------################----------------------------------
+
+    // NOVAS CHAVES (Entrada para Estoque)
+    public static final String EXCHANGE_ESTOQUE = "appointments.estoque";
+    public static final String ROUTING_KEY_ESTOQUE = "appointments.confirmed.estoque";
+    public static final String QUEUE_ESTOQUE = "estoque.resource.input"; // Nova Fila de Recebimento
+
+    //NOVA Fila de Recebimento
+    @Bean
+    public Queue estoqueQueue() {
+        return new Queue(QUEUE_ESTOQUE, true); // true = fila persistente
+    }
+
+    //NOVO Exchange (usando TopicExchange, que é mais flexível para chaves diversas)
+    @Bean
+    public Exchange estoqueExchange() {
+        return new TopicExchange(EXCHANGE_ESTOQUE);
+    }
+
+    //Liga a NOVA Fila de Recebimento ao NOVO Exchange
+    @Bean
+    public Binding estoqueBinding(Queue estoqueQueue, TopicExchange estoqueExchange) {
+        // A fila é ligada usando a Routing Key completa
+        return BindingBuilder.bind(estoqueQueue).to(estoqueExchange).with(ROUTING_KEY_ESTOQUE);
+    }
+
+    // -----------------------------################----------------------------------
+
+
 }
